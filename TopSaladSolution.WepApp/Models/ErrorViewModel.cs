3e94a04690a0a322596924b5ea3b230140442e9b
Phuoc Chung <chungthanhphuoc@hotmail.com>
2023-12-21 10:07:31 +0700

    Add project files.

---
+++ TopSaladSolution.WepApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TopSaladSolution.WepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

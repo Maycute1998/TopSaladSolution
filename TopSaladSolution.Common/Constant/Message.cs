683deea3912b75054e0a5e76293589e35e5caca4
Phuong Uyen Nguyen <mng18698@gmail.com>
2023-12-29 01:28:12 +0700

    add system message

---
+++ TopSaladSolution.Common/Constant/Message.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TopSaladSolution.Common.Constant
+{
+    public static class Message
+    {
+        public static string CreatedSuccess = "Item created successfully";
+        public static string CreatedFailed = "Failed to insert this item";
+        public static string Removed = "Item has been deleted";
+        public static string RemovedFailed = "Failed to remove this item";
+        public static string Updated = "Item has been updated";
+        public static string UpdatedFailed = "Failed to update this item";
+    }
+}
